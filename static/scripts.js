a8fc94a62dbf2c701d9942be936aa5e65419784f
WillBrown5515 <17wtbrown@gmail.com>
2024-09-20 10:27:42 +0100

    Check unames when registering

--- static/scripts.js
+++ static/scripts.js
@@ -1,68 +1,67 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('JavaScript is working!');
-});
-
-async function check_db() {
-    let supaclient = supabase.createClient('https://srhywkedxssxlsjrholj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNyaHl3a2VkeHNzeGxzanJob2xqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYzOTYxNjUsImV4cCI6MjA0MTk3MjE2NX0.lUZUAm20JIH3aoUxmyCAcr8l-A3_S3FpTaHuljrwm50')
-    supaclient
-    .from('credentials')
-    .select('*')
-    .then(({ data, error }) => {
-      if (error) {
-        console.error('Error fetching credentials:', error.message);
-      } else {
-        console.log('Credentials:', data);
-      }
-    });
-}
-
-async function fetchCredentials() {
-    let supaclient = supabase.createClient('https://srhywkedxssxlsjrholj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNyaHl3a2VkeHNzeGxzanJob2xqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYzOTYxNjUsImV4cCI6MjA0MTk3MjE2NX0.lUZUAm20JIH3aoUxmyCAcr8l-A3_S3FpTaHuljrwm50')
-try {
-    const { data, error } = await supaclient.from('credentials').select('*');
-    if (error) throw error;
-    console.log(data);
-} catch (error) {
-    console.error(error);
-}
-}
-
+// document.addEventListener('DOMContentLoaded', () => {
+//     console.log('JavaScript is working!');
+// });
 
 async function login() {
   let supaclient = supabase.createClient('https://srhywkedxssxlsjrholj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNyaHl3a2VkeHNzeGxzanJob2xqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYzOTYxNjUsImV4cCI6MjA0MTk3MjE2NX0.lUZUAm20JIH3aoUxmyCAcr8l-A3_S3FpTaHuljrwm50')
+
   try {
     let { data , error } = await supaclient.from('credentials').select('username')
+
     if (error) throw error
     regs_unames = []
+
     for ((uname) in (data)) {
       regs_unames.push(data[uname].username)
     }
     let found = regs_unames.indexOf(document.getElementById('uname').value)
-    console.log('found', found)
+
     if (found == -1) {
       // Message saying username not found
       // Deactivate textboxes while things are happening, loading sign?
     } else {
-      console.log("pword", document.getElementById('pword').value)
       let {data, error} = await supaclient.from('credentials').select('passcode')
+
       if (error) throw error
-      console.log('all data', data)
-      console.log('pword data', (data[found]))
       req_pword = data[(found)].passcode
-      console.log('Req_pword', req_pword)
+
       if (document.getElementById('pword').value == req_pword) {
-        console.log("SUCCESS!")
+        // Log in
       } else {
         console.log("FAIL")
+        // Empty pword textbox, error message
       }
     }
-    } catch (error) {
+  } catch (error) {
     console.log(error)
   }
 }
 
 
+async function register() {
+  let supaclient = supabase.createClient('https://srhywkedxssxlsjrholj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNyaHl3a2VkeHNzeGxzanJob2xqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYzOTYxNjUsImV4cCI6MjA0MTk3MjE2NX0.lUZUAm20JIH3aoUxmyCAcr8l-A3_S3FpTaHuljrwm50')
+  let { data , error } = await supaclient.from('credentials').select('username')
+  if (error) throw error
+  regs_unames = []
 
+  for ((uname) in (data)) {
+    regs_unames.push(data[uname].username)
+  }
+  let found = regs_unames.indexOf(document.getElementById('reg-uname').value)
+  if (found == -1) {
+    console.log("REG")
+    // passcode = assign_passcode()
+    // Register
 
-// fetchCredentials()
-check_db()
+  const { data, error } = await supabase
+  .from('credentials')
+  .insert([
+    { usenname: (document.getElementById('reg-uname')), other_column: 'otherValue' },
+  ])
+  .select()
+  } else {
+    console.log("NAH")
+    // Already in use
+  }
+
+}
