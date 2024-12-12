a5303897fd8ee3abf32e2bbeda06eda422229dcf
WillBrown5515 <17wtbrown@gmail.com>
2024-12-12 18:06:50 +0000

    Update scorer to accommodate changes with more leagues.

--- python/add_scores.py
+++ python/add_scores.py
@@ -3,50 +3,59 @@ import supabase
 
 class calc_scores:
 
-    def init(self, supabase, foot_api):
+    def __init__(self, supabase, leagues):
         self.supabase = supabase
-        self.foot_api = foot_api
-
+        self.leagues = leagues
 
     def calculate_scores(self, uname):
-        data = self.supabase.table('Predictions').select('*').eq('username', uname)
-        preds = [value for key, value in data.data[0].items() if key != 'username']
 
-        url = f"https://api.football-data.org/v4/competitions/PL/standings?season=2024"
-        headers = {
-            "X-Auth-Token": self.foot_api
-        }
-        response = requests.get(url, headers=headers)
+        preds = [None] * 6
+        data = [None] * 6
+        standings = [None] * 6
+        team_standings = [None] * 6
+        self.points = [None] * 6
 
-        if response.status_code == 200:
-            stands = response.json()
-            standings = stands['standings']
+        for league in range(len(self.leagues)):
+            data[league] = self.supabase.table(self.leagues[league].shorthand + "_preds").select('*').eq('username', uname).execute()
+            preds[league] = [value for key, value in data[league].data[0].items() if key!= 'username']
 
-            team_standings = []
-            for team in standings:
-                for individ_team in team['table']:
-                    team_name = individ_team['team']['name']
-                    team_standings.append(team_name)
+            standings[league] = self.supabase.table(self.leagues[league].shorthand + '_preds').select('*').eq('username', 'standings').execute()
+            team_standings[league] = [value for key, value in standings[league].data[0].items() if key!= 'username']
 
-        preds = ['MCI','MUN','ARS','LIV','NEW','CHE','AVL','TOT','BRE','BRI','CRY','WHU','EVE','NOT','LUT','WOL','BUR','BOU','FUL','SHU']
-        team_standings = ['MCI','ARS','LIV','AVL','TOT','CHE','NEW','MUN','WHU','CRY','BRI','BOU','FUL','WOL','EVE','BRE','NOT','LUT','BUR','SHU',]
 
-        # create points list, using the premier league scoring function
-        points = self.premier_league_scoring(preds, team_standings)
+        ### Series of example teams for testing purposes - comment out for loop above
+        # preds[0] = ['MCI','MUN','ARS','LIV','NEW','CHE','AVL','TOT','BRE','BRI','CRY','WHU','EVE','NOT','LUT','WOL','BUR','BOU','FUL','SHU']
+        # team_standings[0] = ['MCI','ARS','LIV','AVL','TOT','CHE','NEW','MUN','WHU','CRY','BRI','BOU','FUL','WOL','EVE','BRE','NOT','LUT','BUR','SHU',]
+
+        # preds[1] = ['Athletic Club', 'CA Osasuna', 'CD Leganés', 'Club Atlético de Madrid', 'Deportivo Alavés', 'FC Barcelona', 'Getafe CF', 'Girona FC', 'RC Celta de Vigo', 'RCD Espanyol de Barcelona', 'RCD Mallorca', 'Rayo Vallecano de Madrid', 'Real Betis Balompié', 'Real Madrid CF', 'Real Sociedad de Fútbol', 'Real Valladolid CF', 'Sevilla FC', 'UD Las Palmas', 'Valencia CF', 'Villarreal CF']
+        # team_standings[1] = ['FC Barcelona', 'Real Madrid CF', 'Club Atlético de Madrid', 'Athletic Club', 'Villarreal CF', 'Real Sociedad de Fútbol', 'CA Osasuna', 'RCD Mallorca', 'Girona FC', 'RC Celta de Vigo', 'Real Betis Balompié', 'Rayo Vallecano de Madrid', 'Sevilla FC', 'UD Las Palmas', 'Deportivo Alavés', 'CD Leganés', 'Getafe CF', 'RCD Espanyol de Barcelona', 'Valencia CF', 'Real Valladolid CF']
+
+        # preds[2] = ['Blackburn Rovers FC', 'Bristol City FC', 'Burnley FC', 'Cardiff City FC', 'Coventry City FC', 'Derby County FC', 'Hull City AFC', 'Leeds United FC', 'Luton Town FC', 'Middlesbrough FC', 'Millwall FC', 'Norwich City FC', 'Oxford United FC', 'Plymouth Argyle FC', 'Portsmouth FC', 'Preston North End FC', 'Queens Park Rangers FC', 'Sheffield United FC', 'Sheffield Wednesday FC', 'Stoke City FC', 'Sunderland AFC', 'Swansea City AFC', 'Watford FC', 'West Bromwich Albion FC']
+        # team_standings[2] = ['Sheffield United FC', 'Leeds United FC', 'Burnley FC', 'Sunderland AFC', 'Middlesbrough FC', 'Blackburn Rovers FC', 'Watford FC', 'West Bromwich Albion FC', 'Sheffield Wednesday FC', 'Norwich City FC', 'Millwall FC', 'Bristol City FC', 'Swansea City AFC', 'Coventry City FC', 'Stoke City FC', 'Derby County FC', 'Preston North End FC', 'Luton Town FC', 'Oxford United FC', 'Queens Park Rangers FC', 'Cardiff City FC', 'Plymouth Argyle FC', 'Portsmouth FC', 'Hull City AFC']
+
+        # preds[3] = ['AC Milan', 'AC Monza', 'ACF Fiorentina', 'AS Roma', 'Atalanta BC', 'Bologna FC 1909', 'Cagliari Calcio', 'Como 1907', 'Empoli FC', 'FC Internazionale Milano', 'Genoa CFC', 'Hellas Verona FC', 'Juventus FC', 'Parma Calcio 1913', 'SS Lazio', 'SSC Napoli', 'Torino FC', 'US Lecce', 'Udinese Calcio', 'Venezia FC']
+        # team_standings[3] = ['Atalanta BC', 'SSC Napoli', 'FC Internazionale Milano', 'ACF Fiorentina', 'SS Lazio', 'Juventus FC', 'AC Milan', 'Bologna FC 1909', 'Empoli FC', 'Udinese Calcio', 'AS Roma', 'Torino FC', 'Parma Calcio 1913', 'Genoa CFC', 'Cagliari Calcio', 'US Lecce', 'Como 1907', 'Hellas Verona FC', 'AC Monza', 'Venezia FC']
+
+        # preds[4] = ['1. FC Heidenheim 1846', '1. FC Union Berlin', '1. FSV Mainz 05', 'Bayer 04 Leverkusen', 'Borussia Dortmund', 'Borussia Mönchengladbach', 'Eintracht Frankfurt', 'FC Augsburg', 'FC Bayern München', 'FC St. Pauli 1910', 'Holstein Kiel', 'RB Leipzig', 'SC Freiburg', 'SV Werder Bremen', 'TSG 1899 Hoffenheim', 'VfB Stuttgart', 'VfL Bochum 1848', 'VfL Wolfsburg']
+        # team_standings[5] = ['FC Bayern München', 'Eintracht Frankfurt', 'Bayer 04 Leverkusen', 'RB Leipzig', 'VfL Wolfsburg', 'Borussia Dortmund', 'SC Freiburg', 'VfB Stuttgart', '1. FSV Mainz 05', 'SV Werder Bremen', 'Borussia Mönchengladbach', '1. FC Union Berlin', 'FC Augsburg', 'TSG 1899 Hoffenheim', 'FC St. Pauli 1910', '1. FC Heidenheim 1846', 'Holstein Kiel', 'VfL Bochum 1848']
+
+        # preds[5] = ['AJ Auxerre', 'AS Monaco FC', 'AS Saint-Étienne', 'Angers SCO', 'FC Nantes', 'Le Havre AC', 'Lille OSC', 'Montpellier HSC', 'OGC Nice', 'Olympique Lyonnais', 'Olympique de Marseille', 'Paris Saint-Germain FC', 'RC Strasbourg Alsace', 'Racing Club de Lens', 'Stade Brestois 29', 'Stade Rennais FC 1901', 'Stade de Reims', 'Toulouse FC']
+        # team_standings[5] = ['Paris Saint-Germain FC', 'Olympique de Marseille', 'AS Monaco FC', 'Lille OSC', 'Olympique Lyonnais', 'OGC Nice', 'Racing Club de Lens', 'AJ Auxerre', 'Stade de Reims', 'Toulouse FC', 'Stade Brestois 29', 'Stade Rennais FC 1901', 'FC Nantes', 'RC Strasbourg Alsace', 'Angers SCO', 'AS Saint-Étienne', 'Le Havre AC', 'Montpellier HSC']
 
         # We have three lists: 
         # preds, the user's predictions
         # team_standings, the actual prem standings
         # points, the relevant points for each team, equal to the column in the spreadsheet
 
-        columns = []
-        for i in range(1,21):
-            columns.append(str(i))
+        ##### Stuff below is to be completed - submits scores back to database
+        # columns = []
+        # for i in range(1,21):
+        #     columns.append(str(i))
 
-        update_data = dict(zip(columns, points))
+        # update_data = dict(zip(columns, points))
 
-        # Submit the scores into a new 
-        submit = self.supabase.table('scores').update(update_data).match({'username': uname})
+        # # Submit the scores into a new 
+        # submit = self.supabase.table('scores').update(update_data).match({'username': uname})
 
 
     def premier_league_scoring(self, preds, team_standings):
@@ -92,15 +101,13 @@ class calc_scores:
             points[4] = 1
 
         # sixth to sixteenth place
-        i = 5
-        while i < 16:
+        for i in range(5, 16):
             if preds[i] == team_standings[i]:
                 points[i] = 3
             elif preds[i] == team_standings[i-1]:
                 points[i] = 1
             elif preds[i] == team_standings[i+1]:
                 points[i] = 1
-            i += 1
 
         # seventeenth place
         if preds[16] == team_standings[16]:
