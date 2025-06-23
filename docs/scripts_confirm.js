7eb091416441ca343921f31ffabe4c2489bb8d34
WillBrown5515 <17wtbrown@gmail.com>
2025-06-23 14:15:31 +0100

    Change capitalisation

--- docs/scripts_confirm.js
+++ docs/scripts_confirm.js
@@ -45,7 +45,7 @@ async function handleConfirmation() {
 
   await user_team_list(id, uname);
 
-  statusEl.textContent = "Account set-up. Click the button below to return to Beat The Bookie"
+  statusEl.textContent = "Account set-up. Click the button below to return to Beat the Bookie"
   redirectBtn.disabled = false;
 }
 
